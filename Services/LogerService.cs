8d115e171b80be449e2480efff60e08ba2423d86
iwaitu <iwaitucn@gmail.com>
2019-08-11 15:41:18 +0800

    添加项目文件。

---
+++ Services/LogerService.cs
@@ -0,0 +1,65 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using gRpcMongoDB;
+using Google.Protobuf.WellKnownTypes;
+using System.Linq;
+using Google.Protobuf;
+using System.IO;
+using System.Collections.Generic;
+
+namespace gRpcDemo
+{
+    public class LogerService : Loger.LogerBase
+    {
+        private readonly ILogger _logger;
+        private readonly LogService _service;
+
+        public LogerService(ILogger<LogerService> logger, LogService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        public override async Task<LogReply> GetLogList(LogRequest request, ServerCallContext context)
+        {
+            var list = await _service.GetList(request.Date, request.Count);
+            var ret = list.Select(p => new LogContent { Id = p.Id.ToString(), Date = Timestamp.FromDateTime(p.Date), Level = p.Level, Logger = p.Logger, Message = p.Message });
+            return new LogReply { Items = { ret.ToArray() } };
+        }
+
+
+
+        public override async Task<LogItemReply> GetLogItem(LogItemRequest request, ServerCallContext context)
+        {
+            var item = await _service.GetById(request.Id);
+            var content = new LogContent {Id = item.Id.ToString(),Date = Timestamp.FromDateTime(item.Date), Level = item.Level, Logger = item.Logger, Message = item.Message };
+            return new LogItemReply { Item = content };
+        }
+
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private static ByteString GetByteString(LogReply item)
+        {
+            using (var ms = new MemoryStream())
+            {
+                item.WriteTo(ms);
+                return ByteString.CopyFrom(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="byteString"></param>
+        /// <returns></returns>
+        private static LogReply GetPersonFromByteString(ByteString byteString)
+        {
+            return LogReply.Parser.ParseFrom(byteString);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gRpcMongoDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+namespace gRpcDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var mongo = new MongoClient(Configuration["MongoDB:ConnectionString"]);
+            var database = mongo.GetDatabase("tcpwebgatewaylogs");
+            services.AddScoped<IMongoDatabase>(_ => database);
+
+            services.AddScoped<LogService>();
+            services.AddGrpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<LogerService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+
+        public static Newtonsoft.Json.Converters.IsoDateTimeConverter DateTimeConverter { get; set; } = new Newtonsoft.Json.Converters.IsoDateTimeConverter
+        {
+            DateTimeFormat = "yyyy-MM-dd HH:mm:ss"
+        };
+    }
+}
